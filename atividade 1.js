85f024d80e46c2ce869cb446bb79bc7e8621c10f
henrique <henriqueacciarito0@gmail.com>
2025-02-19 22:34:34 -0300

    vlw pessoal

---
+++ atividade 1.js
@@ -0,0 +1,6 @@
+var nome="henrique";
+console.log (`ola, meu nome é ${nome}`);
+var idade= 16;
+var altura= 1.80;
+console.log (`olá meu nome é ${nome}\n e minha idade é ${idade} e minha altura é ${altura}`);
+//entrada de dados
---
+++ var num1=5;.js
@@ -0,0 +1,23 @@
+var num1=43;
+var num2=7;
+var soma =(num1+num2);
+console.log(soma);
+var subtracao
+var multiplicacao
+var divisao
+var num3=15
+var num4=10
+var subtracao = (num3 - num4)
+console.log(subtracao)
+var num5=5
+var num6=3
+var multiplicacao = (num5 * num6)
+console.log(multiplicacao)
+var num7=35
+var num8=35
+var divisao = (num7 / num8)
+console.log(divisao)
+var raiz_quadrada = Math.sqrt(25);
+console.log(raiz_quadrada);
+var resto = (num1 % num2);
+console.log (resto);
